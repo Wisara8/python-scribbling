b80f581a349158330de6729f61184ed3395dcc22
wisara88 <srandall845@gmail.com>
2021-05-29 01:32:11 -0700

    user input

--- variables.py
+++ variables.py
@@ -6,15 +6,24 @@
 
 # print(greeting)
 
-name = "Bob"
-greeting = "Hello, {}"
+# name = "Bob"
+# greeting = "Hello, {}"
 
-with_name = greeting.format(name)
-with_name_two = greeting.format("Rolf")
+# with_name = greeting.format(name)
+# with_name_two = greeting.format("Rolf")
 
-print(with_name)
-print(with_name_two)
+# print(with_name)
+# print(with_name_two)
 
-longer_phrase = "Hello {}, Today is {}"
-formatted = longer_phrase.format(name, "Monday")
-print(formatted)
+# longer_phrase = "Hello {}, Today is {}"
+# formatted = longer_phrase.format(name, "Monday")
+# print(formatted)
+
+# name = input("Enter your name: ")
+
+# print(name)
+
+size_input = input("Size in square feet: ")
+size_input = int(size_input)
+size_meter = size_input / 10.8
+print(f"this many {size_meter}")
