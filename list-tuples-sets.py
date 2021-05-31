63e8dd6a4d57b76386e602bbd1d2dd5fb0402089
wisara88 <srandall845@gmail.com>
2021-05-30 17:51:13 -0700

    joins

--- list-tuples-sets.py
+++ list-tuples-sets.py
@@ -5,10 +5,22 @@ setExmp = {"list", "bob", "Sally"}
 # cannot use subscript notation
 # can add to a set, can't have duplicates
 
-print(listExmp[0])
+# print(listExmp[0])
 listExmp[0] = "Smith"
-print(listExmp[0])
+# print(listExmp[0])
 
 listExmp.append("Darryl")
 listExmp.remove("bob")
-print(listExmp)
+# print(listExmp)
+
+friends = {"Bob", "Rolfe", "Anne"}
+classmates = {"Bob", "Anne", "Cassie"}
+abroad = {"Bob", "Anne"}
+others = {"Jim"}
+
+local_friends = friends.difference(abroad)
+print(local_friends)
+people = friends.union(others)
+print(people)
+both = friends.intersection(classmates)
+print(both)
