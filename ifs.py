0dd31a8f8fd10af3761ab40385ab1cb14f934c30
wisara88 <srandall845@gmail.com>
2021-05-30 18:06:48 -0700

    ifs

---
+++ ifs.py
@@ -0,0 +1,10 @@
+day_of_the_week = input("what day is it today? ").lower()
+
+if day_of_the_week == "Monday":
+    print("Have a good week")
+elif day_of_the_week == "Tuesday":
+    print("Tuesday")
+else:
+    print("Full steam ahead")
+
+print("always printed")
