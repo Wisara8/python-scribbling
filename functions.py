301b6a8d200d52a2886a1d4676d8ef3f042a17e1
wisara88 <srandall845@gmail.com>
2021-05-31 20:18:58 -0700

    age function

--- functions.py
+++ functions.py
@@ -1,4 +1,12 @@
-student_attendance = {"Rolf": 96, "Bob": 77, "Karen": 55}
+# student_attendance = {"Rolf": 96, "Bob": 77, "Karen": 55}
 
-for student, attendance in student_attendance.items():
-    print(f"{student} attended {attendance} classes")
+# for student, attendance in student_attendance.items():
+#     print(f"{student} attended {attendance} classes")
+
+def age():
+    user_age = int(input("what is your age? "))
+    age_seconds = user_age * 365 * 24 * 60 * 60
+    print(f"Your age in seconds is {age_seconds}")
+
+
+age()
