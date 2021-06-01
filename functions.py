57bb7c9eee6a9105488ee21913ed2febd1c25422
wisara88 <srandall845@gmail.com>
2021-05-31 20:31:01 -0700

    function notes

--- functions.py
+++ functions.py
@@ -3,10 +3,18 @@
 # for student, attendance in student_attendance.items():
 #     print(f"{student} attended {attendance} classes")
 
-def age():
-    user_age = int(input("what is your age? "))
-    age_seconds = user_age * 365 * 24 * 60 * 60
-    print(f"Your age in seconds is {age_seconds}")
+# def age():
+#     user_age = int(input("what is your age? "))
+#     age_seconds = user_age * 365 * 24 * 60 * 60
+#     print(f"Your age in seconds is {age_seconds}")
 
 
-age()
+# age()
+
+def sum(x, y):
+    return x + y
+
+
+result = sum(5, y=8)
+# note you can't do the opposite of defining x and not y; if the first is declared the rest must be too
+print(result)
