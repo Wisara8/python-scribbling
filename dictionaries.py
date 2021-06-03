770f353c23a4ebef5bc1ad84f09b36107689e7d6
wisara88 <srandall845@gmail.com>
2021-06-02 21:59:52 -0700

    unpacking dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+users = [
+    (0, "Bob", "password"),
+    (1, "Rolfe", "Rolfe123"),
+    (2, "Jose", "Noway")
+]
+
+username_mapping = {user[1]: user for user in users}
+
+print(username_mapping)
+
+username_input = input("enter username: ")
+userpassword_input = input("enter password: ")
+
+_, username, password = username_mapping[username_input]
+
+if password == userpassword_input:
+    print("correct")
+else:
+    print("wrong")
