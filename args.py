0fc70e54b1e868f5f1b3d00fe4a3efaf462d5480
wisara88 <srandall845@gmail.com>
2021-06-04 20:25:35 -0700

    error fixed

--- args.py
+++ args.py
@@ -1,9 +1,26 @@
 def multiply(*args):
-    print(args)
+    # print(args)
     total = 1
     for arg in args:
         total = total * arg
     return total
 
+# def add(x, y):
+#     total = x + y
+#     return total
 
-print(multiply(1, 3, 5))
+
+# num = {'x': 5, 'y': 3}
+# print(add(**num))
+
+def apply(*args, operator):
+    if operator == '+':
+        return sum(args)
+    elif operator == '*':
+        # print("apply")
+        return multiply(*args)
+    else:
+        return "Not computed"
+
+
+print(apply(1, 3, 5, operator='*'))
