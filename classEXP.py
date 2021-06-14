4c4fbeea08f51a7cc444ee615c6742bf603d3fcd
wisara88 <srandall845@gmail.com>
2021-06-13 17:34:50 -0700

    magic method

--- classEXP.py
+++ classEXP.py
@@ -1,12 +1,16 @@
 class Student:
-    def __init__(self):
-        self.name = "Rolfe"
-        self.grades = (90, 95, 88, 74, 98)
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
+
+    def __str__(self):
+        return f'{self.name} has grades {self.grades}'
 
     def average_grade(self):
         return sum(self.grades) / len(self.grades)
 
 
-student = Student()
-
+student = Student("Bob", (90, 95, 88, 74, 98))
+print(student)
+print(student.name)
 print(student.average_grade())
