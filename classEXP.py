28066e428c12ac0547274d4a985487229beff9e4
wisara88 <srandall845@gmail.com>
2021-06-13 17:21:38 -0700

    average with class

--- classEXP.py
+++ classEXP.py
@@ -1,8 +1,12 @@
 class Student:
     def __init__(self):
         self.name = "Rolfe"
+        self.grades = (90, 95, 88, 74, 98)
+
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades)
 
 
 student = Student()
 
-print(student.name)
+print(student.average_grade())
