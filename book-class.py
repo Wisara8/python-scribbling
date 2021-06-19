b3e0dda1b5227e599acf2198b84fbaef582ee11a
wisara88 <srandall845@gmail.com>
2021-06-18 19:53:28 -0700

    proper notation

--- book-class.py
+++ book-class.py
@@ -11,9 +11,16 @@ class Book:
 
     @classmethod
     def hardcover(cls, name, page_weight):
-        return Book(name, Book.TYPES[0], page_weight + 100)
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name, page_weight):
+        return cls(name, cls.TYPES[1], page_weight)
 
 
 book = Book.hardcover("Harry Potter", 1500)
+book_light = Book.paperback("Python 101", 900)
+
 
 print(book)
+print(book_light)
