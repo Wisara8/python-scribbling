2644cbb28343989e00fde82c3ce92fba9a56a829
wisara88 <srandall845@gmail.com>
2021-06-21 23:50:03 -0700

    try except exp

--- type-imports.py
+++ type-imports.py
@@ -3,7 +3,9 @@ from errorExp import divide
 print("welcome to divisor programe")
 
 grades = []
-
-average = divide(sum(grades), len(grades))
+try:
+    average = divide(sum(grades), len(grades))
+except ZeroDivisionError:
+    print("no grades yet")
 
 print(f"average grade is {average}")
