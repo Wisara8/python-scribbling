25e36d5cc5768aa06d147f60e64ffe67ce0fbe88
wisara88 <srandall845@gmail.com>
2021-06-21 23:45:13 -0700

    imports and errors

---
+++ errorExp.py
@@ -0,0 +1,5 @@
+def divide(dividend, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0")
+
+    return dividend / divisor
--- type-imports.py
+++ type-imports.py
@@ -1,1 +1,9 @@
-# placeholder
+from errorExp import divide
+
+print("welcome to divisor programe")
+
+grades = [1, 5, 4]
+
+average = divide(sum(grades), len(grades))
+
+print(f"average grade is {average}")
