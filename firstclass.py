864cd28f20e4c55441c71068d89ba64459b0e3c3
wisara88 <srandall845@gmail.com>
2021-06-25 22:28:57 -0700

    setup

--- firstclass.py
+++ firstclass.py
@@ -1,13 +1,16 @@
-def divide(dividend, divisor):
-    if divisor == 0:
-        raise ZeroDivisionError("Divisor can't be zero")
+# EXP 1
+# def divide(dividend, divisor):
+#     if divisor == 0:
+#         raise ZeroDivisionError("Divisor can't be zero")
 
-    return dividend / divisor
-
-
-def calculate(*values, operator):
-    return operator(*values)
+#     return dividend / divisor
 
 
-result = calculate(20, 4, operator=divide)
-print(result)
+# def calculate(*values, operator):
+#     return operator(*values)
+
+
+# result = calculate(20, 4, operator=divide)
+# print(result)
+
+# EXP 2
