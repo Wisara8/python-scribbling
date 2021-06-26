80a60815748136fede8ecd58f334e6fef54d6e22
wisara88 <srandall845@gmail.com>
2021-06-25 22:37:11 -0700

    2nd exp

--- firstclass.py
+++ firstclass.py
@@ -14,3 +14,22 @@
 # print(result)
 
 # EXP 2
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+
+    raise RuntimeError("expected not found")
+
+
+def get_friend_name(friend):
+    return friend["name"]
+
+
+friends = [
+    {"name": "Rolfe Smith", "age": 23},
+    {"name": "Bob Capta", "age": 25},
+    {"name": "Jimmy Sodda", "age": 33}
+]
+
+print(search(friends, "Rolfe Smith", get_friend_name))
