33c759dd0bf9387bb4516ba5849cb2d0d8ce7e7c
wisara88 <srandall845@gmail.com>
2021-07-10 23:52:33 -0700

    .

--- journal/database.py
+++ journal/database.py
@@ -2,10 +2,13 @@ import sqlite3
 
 connection = sqlite3.connect("data.db")
 
+# lsdls
+
 
 def create_table():
     with connection:
-        connection.execute("CREATE TABLE entries (content TEXT, date TEXT);")
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
 
 
 def add_entry(entry_content, entry_date):
