6e8203053b0b2063a5b752ed5a3ab6266a45e32f
wisara88 <srandall845@gmail.com>
2021-07-11 23:49:32 -0700

    syntax

--- journal/database.py
+++ journal/database.py
@@ -14,7 +14,7 @@ def create_table():
 def add_entry(entry_content, entry_date):
     with connection:
         connection.execute(
-            f"INSERT INTO entries VALUES ('{entry_content}', '{entry_date}');"
+            f"INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date)
         )
 
 
