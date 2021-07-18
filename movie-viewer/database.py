0e346fb73cdb25ad187ddd08901124f9be097769
wisara88 <srandall845@gmail.com>
2021-07-17 17:07:22 -0700

    sql queries

--- movie-viewer/database.py
+++ movie-viewer/database.py
@@ -1,4 +1,6 @@
-# title, release_date, watched
+import datetime
+import sqlite3
+
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
   title TEXT,
@@ -13,3 +15,37 @@ SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+
+connection = sqlite3.connect("data.db")
+
+
+def create_tables():
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
+
+
+def add_moves(title, release_timestamp):
+    with connection:
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+
+
+def get_movies(upcoming=False):
+    with connection:
+        cursor = connection.cursor()
+        if upcoming:
+            today_timestamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
+
+
+def get_watched_movies():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
+
+
+def watch_movie():
+    pass
